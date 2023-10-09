f0103e228062fe0b2d78b039da4e16502126a121
David Schneidau <143766314+davnschneidau@users.noreply.github.com>
2023-10-09 12:50:57 -0700

    Update blockchain_structure.py

--- python-blockchain-base/blockchain/blockchain_structure.py
+++ python-blockchain-base/blockchain/blockchain_structure.py
@@ -5,7 +5,7 @@ block = {
         {
             "sender": "8527147fe1f5426f9dd545de4b27ee00",
             "recipient": "a77f5cdfa2934df3954a5c7c7da5df1f",
-            "amount": 5,
+            "message": 'This is a test message',
         }
     ],
     "proof": 324984774000,
